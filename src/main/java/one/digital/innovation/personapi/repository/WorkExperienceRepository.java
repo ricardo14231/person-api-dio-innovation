c0a88d35938d77ea348d9da95aa90b38a55ebc48
Ricardo Farias <ricardo14231@hotmail.com>
2021-07-16 18:20:05 -0300

    Refatoração da arquitetura do projeto.

--- src/main/java/one/digital/innovation/personapi/repository/WorkExperienceRepository.java
+++
@@ -1,9 +0,0 @@
-package one.digital.innovation.personapi.repository;
-
-import one.digital.innovation.personapi.entity.WorkExperience;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface WorkExperienceRepository extends JpaRepository<WorkExperience, Long> {
-}
